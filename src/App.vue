049f124d3fc1303f4ac7f663b0a4887a4f8882e8
Mate Paluska <paluska.mate@gmail.com>
2021-09-29 20:04:13 +0200

    generating and distributing deck

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,92 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <DistributedCards :distributedDeck="distributedDeck" />
+  <img src="./assets/cards/2_of_clubs.svg">
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DistributedCards from './components/DistributedCards.vue'
+//import image from './assets/cards/2_of_clubs.svg'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    DistributedCards
+  },
+
+  data(){
+    return{
+      deck:[
+        {type: ''},
+        {number: ''},
+        {src: ''},
+      ],
+
+      distributedDeck:[
+        {type: ''},
+        {number: ''},
+        {src: ''},
+      ]
+      
+    }
+  },
+
+  methods:{
+
+    generateDeck(){
+
+      this.deck = [];
+      let temporaryDeck = [];
+      let types = ['hearts', 'spades', 'clubs', 'diamonds'];
+      let numbers = [
+        '2', '3', '4', '5', '6', '7', '8', '9', '10',
+        'jack', 'queen', 'king', 'ace'
+      ];
+      let indexes = [];
+      for (let i = 0; i < 52; i++) {
+        indexes.push(i);
+      }
+
+      for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 13; j++) {
+          temporaryDeck.push({
+            type: types[i],
+            number: numbers[j],
+            src: './assets/cards/' + numbers[j] + '_of_' + types[i] + '.svg'
+          })
+        }
+      }
+
+      let deckSize = 51;
+      do{
+        let idx = Math.floor(Math.random() * deckSize);
+        let removedIndex = indexes.splice(idx, 1);
+        this.deck[removedIndex] = temporaryDeck.pop();
+        deckSize -= 1;
+      }while(deckSize >= 0);
+
+      console.log(this.deck);
+
+      //getting distributed cards
+      for(let i = 0; i < 28; i++){
+        this.distributedDeck[i] = this.deck.pop();
+      }
+
+      console.log(this.distributedDeck);
+
+
+    },
+
+  /* computed: {
+    image () {
+      return require('./assets/cards/' + '2_of_clubs' + '.svg');
+    }
+  } */
+
+  },
+
+  created(){
+    this.generateDeck();
   }
 }
 </script>
---
+++ src/components/DistributedCards.vue
@@ -0,0 +1,28 @@
+<template>
+  <h1>yolo</h1>
+</template>
+
+<script>
+
+
+export default {
+    name: 'DistributedCards',
+    props:{
+        deck: Array
+    },
+
+    data(){
+        return{
+            card: ''
+        }
+    },
+
+    methods:{
+        
+    }
+}
+</script>
+
+<style>
+
+</style>
